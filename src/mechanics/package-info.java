1fd305bf06f259a2d8f38db98c442ebe2755cebc
Justin Kunimune <justinkunimune@gmail.com>
2016-07-07 20:49:54 -1000

    What's up, Docs?
    I worked on a python project with really bad documentation, which
    inspired me to write javadocs for everything.

--- src/mechanics/package-info.java
+++ src/mechanics/package-info.java
@@ -19,35 +19,11 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-package mechanics;
-
 /**
- * A simple class to hold universal values and constants.
+ * Deals with all classes involving the gameplay functionality, such as game
+ * objects and spaces.
  * 
  * @author jkunimune
  * @version 1.0
  */
-public class Univ {
-
-	/*length*/
-	public static final double pix = 1.0;			// pixel (base unit)
-	public static final double m = 0.0000008;		// meter
-	public static final double km = 1000*m;			// kilometer
-	public static final double ft = 0.3048*m;		// foot
-	public static final double mi = 5280*ft;		// mile
-	/*time*/
-	public static final double ms = 1.0;			// millisecond (base unit)
-	public static final double s = 1000.0;			// second
-	/*speed*/
-	public static final double c = 299792458*m/s;	// speed of light
-	/*mass*/
-	public static final double kg = Math.pow(2,55);	// kilogram
-	/*energy*/
-	public static final double J = kg*m*m/(s*s);	// Joule
-	public static final double MJ = 1000000*J;		// Megajoule
-	/*volume*/
-	public static final double m3 = m*m*m;			// cubic meter
-	public static final double L = m3/1000;			// Litre
-	public static final double km3 = m3*1000000000;	// cubic kilometer
-
-}
+package mechanics;
